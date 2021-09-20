b141978801268bb2b72549facf87a819cbd90651
Abelardo Lugo <abelardo.lugo@davinci.edu.ar>
2021-09-20 17:54:18 -0300

    Manejo de archivos

--- clase10/src/CreadorDeArchivos.java
+++ clase10/src/CreadorDeArchivos.java
@@ -5,8 +5,8 @@ public class CreadorDeArchivos {
     public static void main(String[] args) {
 
         try {
+
             File archivo = new File("archivo.txt");
-<<<<<<< HEAD
 
             if (archivo.createNewFile()) {
                 System.out.println("Se creó el archivo de texto: " + archivo.getName());
@@ -16,21 +16,15 @@ public class CreadorDeArchivos {
 
             archivo.delete();
 
-=======
             if (archivo.createNewFile()) {
                 System.out.println("Se creó el archivo de texto" + archivo.getName());
             } else {
                 System.out.println("El archivo ya existe");
             }
->>>>>>> 4e872118a16f0753ae77c03be4c01deb7dc000ed
         } catch (IOException e) {
             System.out.println("Ocurrió un error");
             e.printStackTrace();
         }
-<<<<<<< HEAD
-
-=======
->>>>>>> 4e872118a16f0753ae77c03be4c01deb7dc000ed
     }
 
 }
