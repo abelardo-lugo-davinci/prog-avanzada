70af4cc7e501d4c2ebbabadfce89af4fe69a99c5
Abelardo <alugo@DH.digitalhouse.com>
2021-09-20 17:48:54 -0300

    Manejo de archivos

--- clase10/src/CreadorDeArchivos.java
+++ clase10/src/CreadorDeArchivos.java
@@ -6,15 +6,20 @@ public class CreadorDeArchivos {
 
         try {
             File archivo = new File("archivo.txt");
+
             if (archivo.createNewFile()) {
-                System.out.println("Se creó el archivo de texto" + archivo.getName());
+                System.out.println("Se creó el archivo de texto: " + archivo.getName());
             } else {
                 System.out.println("El archivo ya existe");
             }
+
+            archivo.delete();
+
         } catch (IOException e) {
             System.out.println("Ocurrió un error");
             e.printStackTrace();
         }
+
     }
 
 }
